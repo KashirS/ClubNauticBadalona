59c5af1db961646dd531bbd225e450a1383b5ed3
KashirS <kashir.sami.7e2@itb.cat>
2021-05-12 18:46:48 +0200

    NavigationView, menu, navigation and reset the errors

--- app/src/main/java/cat/itb/clubnauticbadalona/activities/MainActivity.java
+++ app/src/main/java/cat/itb/clubnauticbadalona/activities/MainActivity.java
@@ -1,16 +1,28 @@
 package cat.itb.clubnauticbadalona.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.navigation.NavigationView;
 
 import cat.itb.clubnauticbadalona.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    static NavigationView view;
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        view = findViewById(R.id.navigationview);
     }
 }

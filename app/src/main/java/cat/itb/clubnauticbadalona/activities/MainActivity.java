938860b8af09de544af70df68d88c794ca406997
KashirS <kashir.sami.7e2@itb.cat>
2021-05-13 19:05:47 +0200

    ContentMain done, where I have navigationView and top_appbar. Also done LoginFragment navigation to HomeFragment and RegisterFragment with all the verifications. But have an error in HomeFragment putting coordinatorLayout visible.

--- app/src/main/java/cat/itb/clubnauticbadalona/activities/MainActivity.java
+++ app/src/main/java/cat/itb/clubnauticbadalona/activities/MainActivity.java
@@ -1,20 +1,29 @@
 package cat.itb.clubnauticbadalona.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
 import cat.itb.clubnauticbadalona.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     static NavigationView view;
     NavController navController;
+    MenuItem menu_icon;
+    MaterialToolbar materialToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
@@ -23,6 +32,28 @@ public class MainActivity extends AppCompatActivity {
 
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
         navController = navHostFragment.getNavController();
-        view = findViewById(R.id.navigationview);
+       view = findViewById(R.id.navigation_view);
+       view.setNavigationItemSelectedListener(this);
+       hideNavDrawer();
+       materialToolbar = findViewById(R.id.top_appbar);
+       materialToolbar.setOnClickListener(this::onClick);
+    }
+    public static void showNavDrawer(){
+        view.setVisibility(View.VISIBLE);
+    }
+    public static void hideNavDrawer(){
+        view.setVisibility(View.INVISIBLE);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.top_appbar:
+                showNavDrawer();
+        }
     }
 }
